9a47c979d4f7955cc54da69d944d54322c8afcac
Mattheus-gs <mattheusgar@gmail.com>
2025-07-01 10:55:29 -0300

    Delete jquery.js

--- jquery.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('botao1').addEventListener('click', function () {
-      const mensagemDiv = document.getElementById('mensagem');
-      mensagemDiv.textContent = 'A VIDA NÃO É FEITA ERROS, MAS SIM DE FASES DE APRENDIZADO 🔱';
-    });
