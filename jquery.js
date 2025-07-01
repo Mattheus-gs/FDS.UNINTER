523ef068374ad6697b106c4fdd22b595b5321840
Mattheus-gs <mattheusgar@gmail.com>
2025-07-01 10:56:01 -0300

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,4 @@
+document.getElementById('botao1').addEventListener('click', function () {
+      const mensagemDiv = document.getElementById('mensagem');
+      mensagemDiv.textContent = 'A VIDA NÃO É FEITA DE ERROS, MAS SIM DE FASES DE APRENDIZADO 🔱';
+    });
